51795bee922317fa989d69cacbae33b6a5f9df12
Nicole Lun <nicole.lun@gmail.com>
2017-01-17 10:27:59 -0600

    Updated page navigation and layout
    Added “next” and “previous” page buttons.
    Standardized layout across all pages.
    Moved “back to top” button from right to left side of page.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,65 @@
 $(document).ready(function() {
-	$("#top-btn").click(function() {
-		console.log("click");
+	$("button.top").click(function() {
 	  	$("body").animate({
 	    	scrollTop: 0
-	  	}, 800);
+	  	}, 600);
 	});
 });
 
 function screenWidth() {
 	console.log(window.innerWidth);
 }
+
+$(document).ready(function() {
+    var caseList = [
+    'globe.html',
+    'look.html',
+    'waiting.html',
+    'labyrinth.html',
+    'soundscapes.html',
+    'street.html',
+    'triptych.html',
+    'lawndale.html',
+    'abstract.html',
+    'window.html',
+    'light-installation.html',
+    'siphon.html',
+    'bottle.html',
+    'box.html',
+    'wagashi.html',
+    'nssmusa.html',
+    'mcs.html',
+    'yangtze.html',
+    'landscape.html',
+    'dump.html',
+    'odyssey.html',
+    'still.html'
+	];
+
+    current = $(location).attr('pathname').substr($(location).attr('pathname').lastIndexOf("/")+1); // get current page: just last part - page name and extension
+    if ($.inArray(current, caseList) !== -1) { //check if it is in array
+        index = $.inArray(current, caseList);
+        console.log(current);
+        console.log(index);
+    }
+
+    //navigate through pages...
+    $("button.next").click(function() {
+	    if (index < caseList.length - 1) {
+	    	next = caseList[index + 1];
+        } else {
+            next = caseList[0];
+        }
+    	window.location.href = next;
+    });
+
+    $("button.prev").click(function() {
+    	if (index > 0) {
+	    	prev = caseList[index - 1];
+        } else {
+            prev = caseList[caseList.length - 1];
+        }
+    	window.location.href = prev;
+    });
+
+});
